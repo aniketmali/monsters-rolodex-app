a83db97fbe40144a17a3eb90a5424bc1db6b33b3
aniketmali <aniketmali7@gmail.com>
2020-05-01 23:40:17 +0300

    Updates

---
+++ precache-manifest.8a257ecd599667154bc0619a01c8eb0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d58e3c6662b65eaa1c2180dcb91aa9d",
+    "url": "/monsters-rolodex-app/index.html"
+  },
+  {
+    "revision": "a3445cfabf2124c0ad4d",
+    "url": "/monsters-rolodex-app/static/css/main.ff81276d.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex-app/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-app/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3445cfabf2124c0ad4d",
+    "url": "/monsters-rolodex-app/static/js/main.b8bc7590.chunk.js"
+  },
+  {
+    "revision": "523233f166d4d535b586",
+    "url": "/monsters-rolodex-app/static/js/runtime-main.82e875d5.js"
+  }
+]);
